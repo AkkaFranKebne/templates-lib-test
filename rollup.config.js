0f7a888a4ce349be484a6b0aba1913fb0cf14067
AkkaFranKebne <ewa.wrobel@ggmail.com>
2024-06-23 19:04:39 +0200

    css bundling

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("@rollup/plugin-typescript");
 const dts = require("rollup-plugin-dts");
+const postcss = require("rollup-plugin-postcss");
 
 const packageJson = require("./package.json");
 
@@ -27,11 +28,13 @@ export default [
         requireReturnsDefault: "auto",
         }),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts.default()],
+    external: [/\.css$/],
   },
 ];
