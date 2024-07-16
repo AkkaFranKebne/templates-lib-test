e4555fb9711422c7f4cdc863add34eae387f7fde
AkkaFranKebne <ewa.wrobel@ggmail.com>
2024-07-16 16:22:16 +0200

    implementing vite, storybook and tailwind

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps {
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className='bg-sky-500 hover:bg-sky-700'>{props.label}</button>;
 };
 
 export default Button;
